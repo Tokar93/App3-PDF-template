8a8972d5945242cb61f6be682e91c1a5b4f638fd
tokarski-to <tomasz.tokarski93@gmail.com>
2024-06-19 15:48:54 +0200

    Day 24: Add multiple lines

--- main.py
+++ main.py
@@ -14,9 +14,8 @@ for index, row in df.iterrows():
     pdf.set_text_color(100, 100, 100)
     pdf.cell(w=0, h=12, txt=row['Topic'], align='L',
              ln=1, border=0)
-    pdf.line(10, 21, 200, 21)
-
-
+    for i in range(21, 291, 10):
+        pdf.line(10, i, 200, i)
 
     # Set the footer
     pdf.ln(265)
@@ -26,7 +25,8 @@ for index, row in df.iterrows():
 
     for i in range(row['Pages'] - 1):
         pdf.add_page()
-        pdf.line(10, 21, 200, 21)
+        for i in range(21, 291, 10):
+            pdf.line(10, i, 200, i)
         # Set the footer
         pdf.ln(277)
         pdf.set_font(family='Times', style='I', size=8)
